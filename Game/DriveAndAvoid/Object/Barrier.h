e55368d7dbfa1af2438b6e77595fc991563cd6d6
yagi-sora18 <138739113+yagi-sora18@users.noreply.github.com>
2024-01-23 11:11:20 +0900

    GameMainScene.cppに追加
    GameMainScene.cppに追加しました。

--- Game/DriveAndAvoid/Object/Barrier.h
+++ Game/DriveAndAvoid/Object/Barrier.h
@@ -1,5 +1,18 @@
 #pragma once
+
+#include"../Utility/Vector2D.h"
+
 class Barrier
 {
+private:
+	int image;       //バリア画像
+	float life_spamn;//バリアの寿命
+
+public:
+	Barrier();
+	~Barrier();
+
+	void Draw(const Vector2D& location);//画像処理
+	bool IsFinished(float speed);     //寿命が尽きたか？
 };
 
--- Game/DriveAndAvoid/Scene/GameMainScene.cpp
+++ Game/DriveAndAvoid/Scene/GameMainScene.cpp
@@ -1,1 +1,92 @@
 #include "GameMainScene.h"
+#include"../Object/RankingData.h"
+#include"DxLib.h"
+#include<math.h>
+
+GameMainScene::GameMainScene() : high_score(0), back_ground(NULL), barrier_image(NULL), mileage(0), player(nullptr), enemy(nullptr)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		enemy_image[i] = NULL;
+		enemy_count[i] = NULL;
+	}
+}
+
+GameMainScene::~GameMainScene()
+{
+
+}
+
+//初期化処理
+void GameMainScene::Initialize()
+{
+	//高得点値を読み込む
+	ReadHighScore();
+
+	//画像の読み込み
+	back_ground = LoadGraph("Resource/images/back.bmp");
+	barrier_image = LoadGraph("Resource/images/barrier.png");
+	int result = LoadDivGraph("Resource/images/car.bmp", 3, 3, 1, 63, 120, enemy_image);
+
+	//エラーチェック
+	if (back_ground == -1)
+	{
+		throw("Resource/images/back.bmpがありません\n");
+	}
+	if (result == -1)
+	{
+		throw("Resource/images/car.bmpがありません\n");
+	}
+
+	//オブジェクトの生成
+	player = new Player;
+	enemy = new Enemy * [10];
+
+	//オブジェクトの初期化
+	player->Initialize();
+
+	for (int i = 0; i < 10; i++)
+	{
+		enemy[i] = nullptr;
+	}
+}
+
+//更新処理
+eSceneType GameMainScene::Update()
+{
+	//プレイヤーの更新
+	player->Update();
+
+	//移動距離の更新
+	mileage += (int)player->GetSpeed
+	() + 5;
+
+	//敵生成処理
+	if (mileage / 20 % 100 == 0)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			if (enemy[i] == nullptr)
+			{
+				int type = GetRand(3) % 3;
+				enemy[i] = new Enemy(type, enemy_image[type]);
+				enemy[i]->Initialize();
+				break;
+			}
+		}
+	}
+
+	//敵の更新と当たり判定チェック
+	for (int i = 0; i < 10; i++)
+	{
+		if(enemy[i] != nullptr)
+	}
+}
+
+
+
+
+
+
+
+
