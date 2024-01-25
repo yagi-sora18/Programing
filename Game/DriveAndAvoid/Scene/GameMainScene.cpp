85156462f1beb80444f5fc7b83feeeae14bbd9cb
yagi-sora18 <138739113+yagi-sora18@users.noreply.github.com>
2024-01-25 11:12:49 +0900

    Ranking.hに追加
    Ranking.hに追加しました。

--- Game/DriveAndAvoid/Scene/GameMainScene.cpp
+++ Game/DriveAndAvoid/Scene/GameMainScene.cpp
@@ -79,12 +79,185 @@ eSceneType GameMainScene::Update()
 	//敵の更新と当たり判定チェック
 	for (int i = 0; i < 10; i++)
 	{
-		if(enemy[i] != nullptr)
+		if (enemy[i] != nullptr)
+		{
+			enemy[i]->Update(player->GetSpeed());
+
+			//画面買いに行ったら、敵を削除してスコア加算
+			if (enemy[i]->GetLocation().y >= 640.0f)
+			{
+				enemy_count[enemy[i]->GetType()]++;
+				enemy[i]->Finalize();
+				delete enemy[i];
+				enemy[i] = nullptr;
+			}
+
+			//当たり判定の確認
+			if (IsHitCheck(player, enemy[i]))
+			{
+				player->SetActive(false);
+				player->DecreaseHp(-50.0f);
+				enemy[i]->Finalize();
+				delete enemy[i];
+				enemy[i] = nullptr;
+			}
+		}
 	}
+
+	//プレイヤーの燃料化体力が0未満なら、リザルトにする
+	if (player->GetFuel() < 0.0f || player->GetHp() < 0.0f)
+	{
+		return eSceneType::E_RESULT;
+	}
+	return GetNowScene();
 }
 
+//描画処理
+void GameMainScene::Draw() const
+{
+	//背景画像の描画
+	DrawGraph(0,mileage % 480 - 480, back_ground, TRUE);
+	DrawGraph(0, mileage % 480, back_ground, TRUE);
 
+	//敵の描画
+	for (int i = 0; i < 10; i++)
+	{
+		if (enemy[i] != nullptr)
+		{
+			enemy[i]->Draw();
+		}
+	}
 
+	//プレイヤーの描画
+	player->Draw();
+
+	//UIの描画
+	DrawBox(500, 0, 640, 480, GetColor(0, 153, 0), TRUE);
+	SetFontSize(16);
+	DrawFormatString(510, 20, GetColor(0, 0, 0), "ハイスコア");
+	DrawFormatString(560, 40, GetColor(255, 255, 255), "%08d", high_score);
+	DrawFormatString(510, 80, GetColor(0, 0, 0), "よけた数");
+	for (int i = 0; i < 3; i++)
+	{
+		DrawRotaGraph(523 + (i * 50), 120, 0.3, 0, enemy_image[i], TRUE, FALSE);
+		DrawFormatString(510 + (i * 50), 140, GetColor(255, 255, 255), "%03d", enemy_count[i]);
+	}
+	DrawFormatString(510, 200, GetColor(0, 0, 0), "走行距離");
+	DrawFormatString(555, 220, GetColor(255, 255, 255), "%08d", mileage / 10);
+	DrawFormatString(510, 240, GetColor(0, 0, 0), "スピード");
+	DrawFormatString(555, 260, GetColor(255, 255, 255), "%08.1f", player->GetSpeed());
+
+}//バリアの枚数の描画
+for (int i = 0; i < player->GetBarriarCount(); i++) {
+	DrawRotaGraph(520, +i * 25, 340, 0.2f, 0, barrier_image, TRUE, FALSE);
+}
+
+//燃料ゲージの描画
+float fx = 510.0f;
+float fy = 390.0f;
+DrawFormatStringF(fx, fy, GetColor(0, 0, 0), "FUFL METER");
+DrawBoxAA(fx, fy + 20.0f, fx + (player->GetFUEL() * 100 / 20000), fy + 40.0f, GetColor(0, 102, 204), TRUE);
+DrawBoxAA(fx, fy + 20.0f, fx + 100.0f, fy + 40.0f, GetColor(0, 0, 0), FALSE);
+
+//体力ゲージの描画
+fx = 510.0f;
+fy = 430.0f;
+DrawFormatStringF(fx, fy, GetColor(0, 0, 0), "PLAYER HP");
+DrawBoxAA(fx, fy + 20.0f, fx + (player->GetHp() * 100 / 1000), fy + 40.0f, GetColor(255, 0, 0), TRUE);
+DrawBoxAA(fx, fy + 20.0f, fx + 100.0f, fy + 40.0f, GetColor(0, 0, 0), FALSE);
+}
+
+//終了時処理
+void GameMainScene::Finalize()
+{
+	//スコアを計算する
+	int score = (mileage / 10 * 10);
+	for (int i = 0; i < 3; i++)
+	{
+		score += (i + 1) * 50 * enemy_count[i];
+	}
+
+	//リザルトデータの書き込み
+	FILE* fp = nullptr;
+	//ファイルオープン
+	errno_t result = fopen_s(&fp, "Resource/dat/result_data.csv", "w");
+
+	//エラーチェック
+	if (result != 0)
+	{
+		throw("Resource/dat/result_data.csvが開けません\n");
+	}
+
+	//スコアを保存
+	fprintf(fp, "%d,\n", score);
+
+	//避けた数と得点を保存
+	for (int i = 0; i < 3; i++)
+	{
+		fprintf(fp, "%d\n", enemy_count[i]);
+	}
+
+	//ファイルクローズ
+	fclose(fp);
+
+	//動的確保したオブジェクトを削除する
+	player->Finalize();
+	delete player;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (enemy[i] != nullptr)
+		{
+			enemy[i]->Finalize();
+			delete enemy[i];
+			enemy[i] = nullptr;
+		}
+	}
+
+	delete[] enemy;
+}
+
+//現在のシーン情報を取得
+eSceneType GameMainScene::GetNowScene() const
+{
+	return eSceneType::E_MAIN;
+}
+
+//ハイスコアの読み込み
+void GameMainScene::ReadHighScore()
+{
+	RankingData data;
+	data.Initialize();
+
+	high_score = data.GetScore(0);
+
+	data.Finalize();
+}
+
+//当たり判定処理(プレイヤーと敵)
+bool GameMainScene::IsHitCheck(Player* p, Enemy* e)
+{
+	//プレイヤーがバリアを貼っていたら、当たり判定を無視する
+	if (p->IsBarrier())
+	{
+		return false;
+	}
+
+	//敵情報がなければ、当たり判定を無視する
+	if (e == nullptr)
+	{
+		return false;
+	}
+
+	//位置情報の差分を取得
+	Vector2D diff_location = p->GetLocation() - e->GetLocation();
+
+	//当たり判定サイズの大きさを取得
+	Vector2D box_ex = p->GetBoxSize() + e->GetBoxSize();
+
+	//コリジョンデータより情報の差分が小さいなら、ヒット判定とする
+	return((fabsf(diff_location.x) < box_ex.x) && (fabsf(diff_location.y) < box_ex.y));
+}
 
 
 
--- Game/DriveAndAvoid/Scene/HelpScene.h
+++ Game/DriveAndAvoid/Scene/HelpScene.h
@@ -1,5 +1,21 @@
 #pragma once
-class HelpScene
+
+#include"SceneBase.h"
+
+class HelpScene : public SceneBase
 {
+private:
+	int background_image; //”wŒi‰æ‘œ
+
+public:
+	HelpScene();
+	virtual ~HelpScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eSceneType GetNowScene() const override;
 };
 
