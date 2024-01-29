244cd300fd7b2dfed009c0159beff2907c6cae4e
yagi-sora18 <138739113+yagi-sora18@users.noreply.github.com>
2024-01-29 15:01:23 +0900

    cppに追加
    cppに追加しました。

--- Game/DriveAndAvoid/Scene/ResultScene.h
+++ Game/DriveAndAvoid/Scene/ResultScene.h
@@ -1,5 +1,27 @@
 #pragma once
-class ResultScene
+
+#include"SceneBase.h"
+
+class ResultScene : public SceneBase
 {
+private:
+	int back_ground;       //”wŒi‰æ‘œ
+	int score;               //ƒXƒRƒA
+	int enemy_count[3];      //“G‚ÌƒJƒEƒ“ƒg
+	int enemy_image[3];      //“G‰æ‘œ
+
+public:
+	ResultScene();
+	virtual ~ResultScene();
+
+	virtual void Initialize() override;
+	virtual eSceneType Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eScene Type GetNowScene() const override;
+
+private:
+	void ReadResultData();
 };
 
