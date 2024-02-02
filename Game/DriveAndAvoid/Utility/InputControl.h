308bf2789d26c808dee376f6fb9731c8b0da2edf
yagi-sora18 <138739113+yagi-sora18@users.noreply.github.com>
2024-02-02 15:00:53 +0900

    cppに追加
    cppに追加しました。

--- Game/DriveAndAvoid/Utility/InputControl.h
+++ Game/DriveAndAvoid/Utility/InputControl.h
@@ -1,5 +1,14 @@
 #pragma once
+
+#include"Vector2D.h"
+
+//入力管理機能
 class InputControl
 {
+private:
+	static bool now_button[16];//現在フレーム入力値
+	static bool old_button[16];//過去フレーム入力値
+	static float trigger[2];   //左右トリガー入力値
+	static Vector2D stick[2];  //左右スティック入力値
 };
 
